c04542265ce37229812f2256c3ff235d645607fc
Ashish Jain <ashishfarnish135@gmail.com>
2026-06-14 16:25:04 +0530

    what is provider

---
+++ interviewQuestion/terraform.tf
@@ -0,0 +1,6 @@
+//what is terraform?
+//Terraform is an open-source infrastructure as code (IaC) tool that allows you to define and provision infrastructure resources using a declarative configuration language. It enables you to manage and automate the deployment of cloud resources, such as virtual machines, storage accounts, and networking components, across various cloud providers like AWS, Azure, Google Cloud, and more. With Terraform, you can version control your infrastructure, collaborate with team members, and ensure consistent and repeatable deployments.
+
+//what is provider in terraform?
+//In Terraform, a provider is a plugin that allows Terraform to interact with a specific cloud provider or service. Providers are responsible for understanding the APIs of the services they manage and translating Terraform configurations into API calls to create, update, or delete resources. Each provider has its own set of resources and data sources that you can use in your Terraform configurations. For example, the "azurerm" provider is used to manage resources in Microsoft Azure, while the "aws" provider is used for Amazon Web Services.
+
