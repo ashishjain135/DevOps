2babe89b1768ad8e33aa21a7eb23ee60b92af5c7
Ashish Jain <ashishfarnish135@gmail.com>
2026-06-19 14:07:22 +0530

    variable declare types all six ways

---
+++ variable190626/variable.tf
@@ -0,0 +1,9 @@
+//first variable.tf file created to define the variable for resource group name.
+//created by a CLI method to define the variable in variable.tf file and call it in main.tf file using var.rg_name 
+
+
+//second -> default variable.tf file created to define the variable for resource group name. 
+//we can define the default value for the variable in variable.tf file and call it in main.tf file using var.ashish
+variable "ashish"{
+    default = "defaultrg1"
+}
