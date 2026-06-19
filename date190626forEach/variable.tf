33267572f97a7e332593b719fb255f1e4c4edda4
Ashish Jain <ashishfarnish135@gmail.com>
2026-06-19 22:55:40 +0530

    for_each with list and map for create multiple resource group

---
+++ date190626forEach/variable.tf
@@ -0,0 +1,3 @@
+//for_each is meta argument which is used to create multiple resources of same type with different values. It is used when we have a map or set of strings. It is similar to count but it is more flexible than count.
+
+variable "ashish" {}
